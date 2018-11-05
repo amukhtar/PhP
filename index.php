2af54ca5ff73d3b84c66adf08604add632753660
amukhtar <amukhtar@users.noreply.github.com>
2018-11-05 14:27:54 -0500

    no message

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 style="text-align:center;">MTUniversity</h1>
+<p style="text-align:center;">Coming soon........</p>
+<p>Upcoming courses</p>
+<?php
+$classes = array("1- Intro to Abrasives 100", "2- Grinding Processes 201", "3- Grinding Safety 211");
+sort($classes);
+
+$clength = count($classes);
+for($x = 0; $x < $clength; $x++) {
+    echo $classes[$x];
+    echo "<br>";
+}
+?>
+
+</body>
+</html>
